0c719978bb17027663e7f8fb7351ff1a7c0e9675
Devin Spikowski <vegeta897@gmail.com>
2023-04-30 22:12:40 -0400

    🔖 Bump overlay websocket data version

--- src/twitch/overlay/websocket.ts
+++ src/twitch/overlay/websocket.ts
@@ -13,7 +13,7 @@ import { getData, modifyData } from '../../db.js'
 import randomstring from 'randomstring'
 import { getOverlayPosition } from '../chat/grace.js'
 
-const version = 1
+const version = 2
 
 export function initWebsocket(server: http.Server) {
 	const authKeys = [...getData().streamOverlayAuthKeys]
