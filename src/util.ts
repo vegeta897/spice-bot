dafe4fdb67929bbc4e62a09e686a193ca6b6b850
Devin Spikowski <vegeta897@gmail.com>
2023-05-02 05:54:32 -0400

    🐛 Well I'm stupid

--- src/util.ts
+++ src/util.ts
@@ -51,7 +51,7 @@ export const compareArrays = (first: unknown[], second: unknown[]) => {
 const randomIntRange = (minOrMax: number, max?: number) => {
 	const min = max === undefined ? 0 : minOrMax
 	const range = max === undefined ? minOrMax : max - minOrMax
-	return Math.floor(min + Math.random() * range)
+	return Math.floor(min + Math.random() * (range + 1))
 }
 
 export const randomElement = <T>(arr: T[]): T =>
