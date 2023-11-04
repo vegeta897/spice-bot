5897a139fe3d99cbbbb209d1a7f5d4ccd90e436d
Devin Spikowski <vegeta897@gmail.com>
2023-11-03 22:38:49 -0400

    🚑 Fix circular dependency

--- src/twitch/chat/whereBot.ts
+++ src/twitch/chat/whereBot.ts
@@ -6,6 +6,41 @@ import { makeTextGraceTrainSafe } from './grace.js'
 import { ChatEvents, sendChatMessage } from './twitchChat.js'
 
 export function initWhereBot() {
+	// TODO: Make this function not depend on Emotes
+	// We get a circular dependency when we try to define this at top-level
+	whereBotReplies = [
+		{ text: "I'm right here!", reply: true },
+		{ text: "I told you, I'm right here!", reply: true },
+		{
+			text: "Hello?! :hello: I'm right here!!!",
+			emotes: [[':hello:', Emotes.BANANA]],
+			reply: true,
+		},
+		{ text: "Okay, you're messing with me.", reply: true },
+		{
+			text: 'How can you bully a poor lil bot like this? :fine:',
+			emotes: [[':fine:', Emotes.THISISFINE]],
+			reply: true,
+		},
+		{
+			text: 'Abby, make them stop! :rage:',
+			emotes: [[':rage:', Emotes.RAGE]],
+		},
+		'Mods? Hello? A little help here?',
+		{
+			text: 'This is just spam, and I will not participate! :sure:',
+			emotes: [[':sure:', Emotes.SURE]],
+		},
+		{
+			text: "I'm a good bot. I'm a good bot. I will not spam. I'm a good bot. :dawg:",
+			emotes: [[':dawg:', Emotes.DAWG]],
+		},
+		{
+			text: 'AAAAAHHHHHHHHHHHHHH :scream:',
+			emotes: [[':scream:', Emotes.SCREAM]],
+		},
+		'/me is gone', // This works without ChatClient.action()
+	]
 	ChatEvents.on('message', (event) => {
 		if (event.self) return
 		if (/where('?|( i))s?( (our|that|dat))? spice[ -]?bot/gi.test(event.text)) {
@@ -51,42 +86,10 @@ async function handleWhereBotPrompt(msg: ChatMessage) {
 	sendChatMessage(...sendMessageArgs)
 }
 
-const whereBotReplies: (
+let whereBotReplies: (
 	| string
 	| { text: string; emotes?: [string, string][]; reply?: boolean }
-)[] = [
-	{ text: "I'm right here!", reply: true },
-	{ text: "I told you, I'm right here!", reply: true },
-	{
-		text: "Hello?! :hello: I'm right here!!!",
-		emotes: [[':hello:', Emotes.BANANA]],
-		reply: true,
-	},
-	{ text: "Okay, you're messing with me.", reply: true },
-	{
-		text: 'How can you bully a poor lil bot like this? :fine:',
-		emotes: [[':fine:', Emotes.THISISFINE]],
-		reply: true,
-	},
-	{
-		text: 'Abby, make them stop! :rage:',
-		emotes: [[':rage:', Emotes.RAGE]],
-	},
-	'Mods? Hello? A little help here?',
-	{
-		text: 'This is just spam, and I will not participate! :sure:',
-		emotes: [[':sure:', Emotes.SURE]],
-	},
-	{
-		text: "I'm a good bot. I'm a good bot. I will not spam. I'm a good bot. :dawg:",
-		emotes: [[':dawg:', Emotes.DAWG]],
-	},
-	{
-		text: 'AAAAAHHHHHHHHHHHHHH :scream:',
-		emotes: [[':scream:', Emotes.SCREAM]],
-	},
-	'/me is gone', // This works without ChatClient.action()
-]
+)[]
 
 let lastWhereBotReplyTime = 0
 const COOLDOWN = 3 * 1000
